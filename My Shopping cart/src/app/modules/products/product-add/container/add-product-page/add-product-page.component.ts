0c72f22ec06d814cdbf494dd3581d10ea88ea879
Atul Gunjal <Atul.Gunjal@BRILLIO.COM>
2022-05-18 15:18:00 +0530

    first commit

---
+++ My Shopping cart/src/app/modules/products/product-add/container/add-product-page/add-product-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-page',
+  templateUrl: './add-product-page.component.html'
+})
+export class AddProductPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
